d014070c8948e4fea6c8ecd4d28cb38ec524e50a
Zeeshan Ali <theblessed.gladiator@gmail.com>
2015-11-15 13:29:22 +0530

    Made container var public
    Added the Masonry controller variable to global scope in theme.js.

---
+++ js/common.js
@@ -0,0 +1,47 @@
+﻿/// <reference path="theme.js" />
+var api_key = "5a49f9bc4403d5816d94e7ff13d464bf";
+var nitsetid = "72157658900187533";
+
+$(document).ready(function () {
+    GetImgs();
+});
+
+function LoopAndFetchImages()
+{
+    $(".galleryImage").each(function () {
+        var imageid = $(this).attr("imageid");
+        if (imageid !== 'undefined' && imageid != null && imageid != "")
+        {
+            GetImgs(imageid, $(this))
+        }
+    });
+}
+
+function GetImgs() {
+    var URL = "https://api.flickr.com/services/rest/" +  // Wake up the Flickr API gods.
+      "?method=flickr.photosets.getPhotos" +  // Get photo from a photoset. http://www.flickr.com/services/api/flickr.photosets.getPhotos.htm
+      "&api_key=" + api_key +  // API key. Get one here: http://www.flickr.com/services/apps/create/apply/
+      "&photoset_id=" + nitsetid +  // The set ID.
+      "&privacy_filter=1" +  // 1 signifies all public photos.
+      "&per_page=100" + // For the sake of this example I am limiting it to 20 photos.
+      "&format=json&nojsoncallback=1" +   // Er, nothing much to explain here.
+      "&extras=original_format"; //original sizes of the images
+
+
+    // See the API in action here: http://www.flickr.com/services/api/explore/flickr.photosets.getPhotos
+    $.getJSON(URL, function (data) {
+        if (data.stat == 'ok') {
+            $.each(data.photoset.photo, function (i, item) {
+                // Creating the image URL. Info: http://www.flickr.com/services/api/misc.urls.html
+                var img_src = "http://farm" + item.farm + ".static.flickr.com/" + item.server + "/" + item.id + "_" + item.secret + "_m.jpg";
+                var img_thumb = $("<img/>").attr("src", img_src).css("margin", "8px")
+                $("img[imageId='" + item.id + "']").attr("src", img_src);
+
+                $container.masonry('layout');
+            });
+
+            
+        }
+    });
+
+}
